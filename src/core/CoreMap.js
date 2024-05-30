b63c752ba7344ab1b4a878aeb5a6ee267cadd8e2
lukeSuperCoder <lc970820@163.com>
2024-05-30 11:24:30 +0800

    添加地图相关配置以及坐标系转换

--- src/core/CoreMap.js
+++ src/core/CoreMap.js
@@ -1,6 +1,7 @@
 import 'ol/ol.css';
 import Map from 'ol/Map';
 import View from 'ol/View';
+import {fromLonLat} from 'ol/proj';
 
 class CoreMap {
     constructor(targetId, options) {
@@ -10,14 +11,15 @@ class CoreMap {
     }
 
     initializeMap() {
+        let that = this;
         this.map = new Map({
             target: this.targetId, // 目标元素的ID
             logo: false,
             loadTilesWhileAnimating: true,
             pixelRatio: 1,
             view: new View({
-                center: [0, 0], // 地图初始中心点，使用EPSG:3857坐标
-                zoom: 2, // 初始缩放级别
+                center: fromLonLat(that.options.center, that.options.projection), // 地图初始中心点，使用EPSG:3857坐标
+                zoom: that.options.zoom, // 初始缩放级别
             }),
         });
         return this.map;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 import 'ol/ol.css';
 import CoreMap from './core/CoreMap';
 import BaseMapManager from './mapManage/BaseMapManager';
+import { transformExtent } from 'ol/proj'
 
 
 export class ElaneOlMap {
     constructor(targetId, options) {
+        let that = this;
+        that.options = {
+            targetId: targetId,
+            baseMapName: 'OSM',
+            center: [116.397428, 39.90923],
+            extent: transformExtent([-360, -90, 360, 90], "EPSG:4326", "EPSG:3857"),
+            zoom: 4,
+            minZoom:1,
+            maxZoom: 18,
+            projection: 'EPSG:3857'
+        }
+        that.options = Object.assign(that.options, options);
         // 创建CoreMap的实例
-        this.coreMap = new CoreMap(targetId, options);
+        this.coreMap = new CoreMap(targetId, that.options);
         if(this.coreMap.map) {
             // 初始化地图底图管理器
             this.mapInstance = this.coreMap.getMapInstance();
-            this.baseMapManager = new BaseMapManager(this.coreMap.map);
+            this.baseMapManager = new BaseMapManager(this.coreMap.map, this.options);
         }
     }
 }
