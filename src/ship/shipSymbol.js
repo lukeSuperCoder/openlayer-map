0a2909e72192c3788b24221bfc845203c36c3c26
lukeSuperCoder <lc970820@163.com>
2024-06-05 13:35:37 +0800

    添加文字标注聚合图层

--- src/ship/shipSymbol.js
+++ src/ship/shipSymbol.js
@@ -1,10 +1,9 @@
-import { Vector as VectorSource } from 'ol/source';
-import { Layer, Vector as VectorLayer } from 'ol/layer';
+import { Vector as VectorSource, Cluster as ClusterSource } from 'ol/source';
+import { Layer, Vector as VectorLayer, Group as LayerGroup } from 'ol/layer';
 import GeoJSON from 'ol/format/GeoJSON.js';
 import WebGLPointsLayer from 'ol/layer/WebGLPoints.js';
-import { transform } from 'ol/proj';
 import Feature from 'ol/Feature.js';
-import {Icon, Style} from 'ol/style.js';
+import {Icon, Style, Text, Fill, Stroke} from 'ol/style.js';
 import {Point} from 'ol/geom';
 /**
  * 船位渲染类
@@ -84,9 +83,7 @@ const shipDetailStyle = {
         100 ,
     ],
 }
-let areaShipLayer = null;
-let areaShipDetailLayer = null;
-let locateLayer = null;
+
 class ShipSymbol {
     constructor(mapInstance, options) {
         let that = this;
@@ -94,19 +91,32 @@ class ShipSymbol {
         this.options = {
 
         }
+        that.areaShipLayer = null;
+        that.areaShipDetailLayer = null;
+        that.areaShipLabelLayer = null;
+        that.locateLayer = null;
+        //创建区域船图层组管理所有区域船相关图层
+        that.areaShipLayerGroup = new LayerGroup({
+            layers: []
+        });
+        that._map.addLayer(that.areaShipLayerGroup);
         that.options = Object.assign(that.options, options);
     }
-    
+    /*
+    * @description 区域船相关
+    */
+    //绘制区域船
     drawAreaShip(data) {
         let that = this;
+        that.removeAreaShip();
         that.drawShipShape(data);
+        that.drawAreaShipLabel(data);
         that._map.on('click', (e) => {
             that._map.forEachFeatureAtPixel(e.pixel, (feature) => {
                 that.locationShip(feature.getGeometry().getCoordinates())
             })
         })
     };
-
     //绘制船形
     drawShipShape(vectorData) {
         let that = this;
@@ -120,43 +130,43 @@ class ShipSymbol {
             wrapX: true,
         });
         //绘制区域船（粗略船形）
-        const preAreaShipLayer = areaShipLayer;
-        areaShipLayer = new WebGLPointsLayer({
+        const preAreaShipLayer = that.areaShipLayer;
+        that.areaShipLayer = new WebGLPointsLayer({
             source: vectorSource,
             style: shipStyle,
-            maxZoom: 8,
+            maxZoom: 16,
             minZoom: 0
         });
-        that._map.addLayer(areaShipLayer);
-
-        if(preAreaShipLayer) {
-            that._map.removeLayer(preAreaShipLayer);
-            preAreaShipLayer.dispose();
-        }
+        that.areaShipLayerGroup.getLayers().push(that.areaShipLayer);
+        //先绘制后清除
+        // if(preAreaShipLayer) {
+        //     that._map.removeLayer(preAreaShipLayer);
+        //     preAreaShipLayer.dispose();
+        // }
 
         //绘制区域船（精细船形）
-        const preAreaShipDetailLayer = areaShipDetailLayer;
-        areaShipDetailLayer = new WebGLPointsLayer({
+        const preAreaShipDetailLayer = that.areaShipDetailLayer;
+        that.areaShipDetailLayer = new WebGLPointsLayer({
             source: vectorSource,
             style: shipDetailStyle,
             maxZoom: 24,
-            minZoom: 8
+            minZoom: 16
         });
-        that._map.addLayer(areaShipDetailLayer);
-
-        if(preAreaShipDetailLayer) {
-            that._map.removeLayer(preAreaShipDetailLayer);
-            preAreaShipDetailLayer.dispose();
-        }
+        that.areaShipLayerGroup.getLayers().push(that.areaShipDetailLayer);
+        // if(preAreaShipDetailLayer) {
+        //     that._map.removeLayer(preAreaShipDetailLayer);
+        //     preAreaShipDetailLayer.dispose();
+        // }
 
     };
-
+    //清除区域船
     removeAreaShip() {
         let that = this;
-        that._map.removeLayer(that.shipLayer);
-        that.shipLayer.dispose();   // 释放资源（这一步必须要加，否则下次绘制会有问题）
+        if(that.areaShipLayerGroup.getLayers().getLength()>0) {
+            that.areaShipLayerGroup.getLayers().clear();
+        }
+        console.log(that.areaShipLayerGroup);
     };
-
     //定位船
     locationShip(point) {
         let that = this;
@@ -198,6 +208,54 @@ class ShipSymbol {
         if(preLocateLayer) {
             that._map.removeLayer(preLocateLayer);
         }
+    }
+    //绘制区域船文字标签
+    drawAreaShipLabel(vectorData) {
+        let that = this;
+        const vectorSource = new VectorSource({
+            features: new GeoJSON().readFeatures(vectorData, {
+                // 数据原始坐标系为EPSG:4326，目标坐标系为EPSG:3857
+                dataProjection: 'EPSG:4326',
+                featureProjection: 'EPSG:3857'
+            }),
+            wrapX: true,
+        });
+        // 聚类源
+        const clusterSource = new ClusterSource({
+            distance: 50, // 聚类距离（像素），根据需要调整
+            source: vectorSource
+        });
+
+        //label样式
+        const styleFunction = function(feature) {
+            const originalFeature = feature.get('features')[0];
+            const style = new Style({
+                text: new Text({
+                  font: '16px sans-serif',
+                  textAlign: 'center',
+                  text: originalFeature.get('city'), // 从原始 feature 获取 tooltip 文本
+                  fill: new Fill({
+                    color: '#000'
+                  }),
+                //   stroke: new Stroke({
+                //     color: '#fff',
+                //     width: 3
+                //   }),
+                  backgroundFill: new Fill({
+                    color: [255,255,255, 0.6],
+                  }),
+                  padding: [2, 2, 2, 2],
+                  offsetX: 50, // 偏移量，以避免与其他点重叠
+                  offsetY: -15
+                })
+            });
+            return style;
+        };
+        that.areaShipLabelLayer = new VectorLayer({
+            source: clusterSource,
+            style: styleFunction
+        });
+        that.areaShipLayerGroup.getLayers().push(that.areaShipLabelLayer);
     }
 }
 export default ShipSymbol;
